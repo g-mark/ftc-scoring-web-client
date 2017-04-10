48252d63342780d31c13edb0ce4d25e6795c6c51
Steven Grosmark <steve@artgig.com>
2017-04-10 11:55:40 -0400

    another older browser fix - added polyfill for Array.prototype.includes

--- public/js/ftc.js
+++ public/js/ftc.js
@@ -1013,7 +1013,6 @@ FTC.prototype = {
 					completion( 200, {header: header, rows: rows} );
 				}
 				else {
-					console.log( 'not modified' );
 					completion( 304 );
 				}
 				self.loading( false );
@@ -1170,13 +1169,65 @@ FTC.prototype = {
 	}
 }
 
-// Polyfill
+// -----> Polyfill
 if (!String.prototype.trim) {
   String.prototype.trim = function () {
     return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
   };
 }
 
+if (!Array.prototype.includes) {
+  // https://tc39.github.io/ecma262/#sec-array.prototype.includes
+  Object.defineProperty(Array.prototype, 'includes', {
+    value: function(searchElement, fromIndex) {
+
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If len is 0, return false.
+      if (len === 0) {
+        return false;
+      }
+
+      // 4. Let n be ? ToInteger(fromIndex).
+      //    (If fromIndex is undefined, this step produces the value 0.)
+      var n = fromIndex | 0;
+
+      // 5. If n ≥ 0, then
+      //  a. Let k be n.
+      // 6. Else n < 0,
+      //  a. Let k be len + n.
+      //  b. If k < 0, let k be 0.
+      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      function sameValueZero(x, y) {
+        return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+      }
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+        // b. If SameValueZero(searchElement, elementK) is true, return true.
+        // c. Increase k by 1. 
+        if (sameValueZero(o[k], searchElement)) {
+          return true;
+        }
+        k++;
+      }
+
+      // 8. Return false
+      return false;
+    }
+  });
+}
+// <----- Polyfill
 
 $(document).ready(function(){
 	/**
